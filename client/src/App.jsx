e7980102d2f9a35c796d90dd749fc8ee04e20b32
GAveroArias <guillermo.avero19@gmail.com>
2024-02-26 19:39:21 -0300

    26Feb3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import HomePage from "/src/components/pages/HomePage.jsx";
+import HomePage from "/src/components/pages/HomeNew.jsx";
 // import DetailPage from "/src/components/pages/DetailPage.jsx";
 // import FormPage from "/src/components/pages/FormPage.jsx";
 import LandingPage from "./components/pages/LandingPage.jsx";
 
 const App = () => {
-  return (
-    <Routes>
-      <Route exact path="/" element={<LandingPage />} />
-      {/* <Route exact path="/home" element={<HomePage />} />
+    return (
+        <Routes>
+            <Route exact path="/" element={<LandingPage />} />
+            <Route exact path="/home" element={<HomePage />} />
+            {/* 
       <Route exact path="/pokemon/:id" element={<DetailPage />} />
       <Route exact path="/form" element={<FormPage />} /> */}
-    </Routes>
-  );
+        </Routes>
+    );
 };
 
 export default App;
---
+++ client/src/components/pages/HomeNew.jsx
@@ -0,0 +1,65 @@
+//En la home lo que vas a hacer es pegarle a /pokemons y obtener todos los pokemons, los guardas en la store y mostras los primeros 12
+//eso significa que vas a tener un useEffect en la home que hace una request por axios a localhost:3001/pokemons, y guarda el resultardo en la store
+//en la store vas a tener un array de pokemons, y vas a tener un useEffect en la home que hace una request por axios a localhost:3001/pokemons, y guarda el resultardo en la store
+//hace eso y cuando lo tengas listo, necesitas paginarlos de 12 en 12 y mostrarlos en tu home. TU código:
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { setPokemons } from "../../redux/actions/pokemonActions";
+import PokemonCard from "../pokemonCard/PokemonCard";
+
+const HomePage = () => {
+    const dispatch = useDispatch();
+    const [pokemons, setPokemons] = useState([]);
+
+    useEffect(() => {
+        const fetchPokemons = async () => {
+            try {
+                const response = await axios.get(
+                    "http://localhost:3001/pokemons"
+                );
+                dispatch(setPokemons(response.data));
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchPokemons();
+    }, [dispatch]);
+
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const indexOfLastPokemon = currentPage * 12;
+    const indexOfFirstPokemon = indexOfLastPokemon - 12;
+
+    const currentPokemons = pokemons.slice(
+        indexOfFirstPokemon,
+        indexOfLastPokemon
+    );
+
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+    return (
+        <div>
+            <h1>Lista de Pokemóns</h1>
+            <PokemonCard pokemons={currentPokemons} />
+            <div>
+                <button
+                    onClick={() => paginate(currentPage - 1)}
+                    disabled={currentPage === 1}
+                >
+                    Anterior
+                </button>
+                <button
+                    onClick={() => paginate(currentPage + 1)}
+                    disabled={currentPokemons.length < 12}
+                >
+                    Siguiente
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
