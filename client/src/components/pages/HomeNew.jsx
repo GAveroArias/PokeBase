32591ff378a30e3904d17a865fdbdf8f24482d2e
GAveroArias <guillermo.avero19@gmail.com>
2024-02-26 21:27:38 -0300

    Hasta aquí vamos

--- client/src/components/pages/HomeNew.jsx
+++ client/src/components/pages/HomeNew.jsx
@@ -14,18 +14,7 @@ const HomePage = () => {
     const [pokemons, setPokemons] = useState([]);
 
     useEffect(() => {
-        const fetchPokemons = async () => {
-            try {
-                const response = await axios.get(
-                    "http://localhost:3001/pokemons"
-                );
-                dispatch(setPokemons(response.data));
-            } catch (error) {
-                console.error(error);
-            }
-        };
-
-        fetchPokemons();
+        dispatch(setPokemons());
     }, [dispatch]);
 
     const [currentPage, setCurrentPage] = useState(1);
